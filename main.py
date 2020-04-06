071eb53ae4f64d1345c02324ac10554ff8f5070d
Monica Turner <turnermon@gmail.com>
2020-04-06 09:36:22 +0200

    Changed output loc and convert to points

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ from utils.hdx_api import query_api
 
 ACAPS_HDX_ADDRESS = 'acaps-covid19-government-measures-dataset'
 
-CRASH_MOVE_MAIN_DIR = os.path.join('2020-03-16-global-covid-19-response-group',
-                                   'GIS',
-                                   '1_Original_Data')
+CRASH_MOVE_MAIN_DIR = os.path.join('2020-03-16-global-covid-19-response-group', 'GIS')
+CRASH_MOVE_INPUT_DIR = '1_Original_Data'
+CRASH_MOVE_OUTPUT_DIR = '2_Active_Data'
 
 ACAPS_DIR = 'ACAPS_Govt_Measures'
 
@@ -41,8 +41,9 @@ def main(cmf_path: str, debug: bool = False):
     df_acaps = get_df_acaps(cmf_path, debug)
     df_acaps_reduced = get_df_acaps_reduced(df_acaps)
     df_naturalearth = get_df_naturalearth(cmf_path)
-    # Do the join
+    # Do the join, and convert to point instead of shape
     df_output = df_naturalearth.merge(df_acaps, how='outer', left_on='ADM0_A3_IS', right_on='ISO')
+    df_output['geometry'] = df_output['geometry'].apply(lambda x: x.representative_point())
     df_output_reduced = df_naturalearth.merge(df_acaps_reduced, how='outer', left_on='ADM0_A3_IS', right_on='ISO')
     # Output to CMF
     output_to_cmf(df_output, df_output_reduced, cmf_path)
@@ -51,7 +52,7 @@ def main(cmf_path: str, debug: bool = False):
 def get_df_acaps(cmf_path: str, debug: bool) -> pd.DataFrame:
     # Get the ACAPS data from HDX
     data_dir = get_temp_dir('covid19_acaps')
-    acaps_dir = os.path.join(cmf_path, CRASH_MOVE_MAIN_DIR, ACAPS_DIR)
+    acaps_dir = os.path.join(cmf_path, CRASH_MOVE_MAIN_DIR, CRASH_MOVE_INPUT_DIR, ACAPS_DIR)
     if not debug:
         filename = list(query_api(ACAPS_HDX_ADDRESS, data_dir).values())[0]
         filepath = os.path.join(acaps_dir, filename)
@@ -66,10 +67,10 @@ def get_df_acaps(cmf_path: str, debug: bool) -> pd.DataFrame:
     # Drop rows with empty region
     df_acaps = df_acaps.loc[df_acaps['REGION'] != '']
     # Make month column and onvert datetime column to string to write to shape file
-    df_acaps['MONTH'] = df_acaps['DATE_IMPLEMENTED'].dt.strftime('%Y/%m')
-    df_acaps['DATE_IMPLEMENTED'] = df_acaps['DATE_IMPLEMENTED'].dt.strftime('%Y/%m/%d')
+    df_acaps['MONTH'] = df_acaps['DATE_IMPLEMENTED'].dt.strftime('%B')
+    df_acaps['DATE_IMPLEMENTED'] = df_acaps['DATE_IMPLEMENTED'].dt.strftime('%Y-%m-%d')
     # Fill NAs in dates with "Unknown"
-    df_acaps.loc[:, ['DATE_IMPLEMENTED', 'MONTH']] = df_acaps.loc[:,['DATE_IMPLEMENTED', 'MONTH']].fillna('Unknown')
+    df_acaps.loc[:, ['DATE_IMPLEMENTED', 'MONTH']] = df_acaps.loc[:, ['DATE_IMPLEMENTED', 'MONTH']].fillna('Unknown')
     return df_acaps
 
 
@@ -85,13 +86,15 @@ def get_df_naturalearth(cmf_path: str) -> gpd.GeoDataFrame:
     # TODO: Fetch NaturalEarth data?
     # Read in NaturalEarth data
     # TODO: Read in from original shapefile and add transformations here
-    input_filename = os.path.join(cmf_path, CRASH_MOVE_MAIN_DIR, NATURAL_EARTH_DIR, NATURAL_EARTH_ZIP_FILENAME)
+    input_filename = os.path.join(cmf_path, CRASH_MOVE_MAIN_DIR, CRASH_MOVE_INPUT_DIR,
+                                  NATURAL_EARTH_DIR, NATURAL_EARTH_ZIP_FILENAME)
     df_naturalearth = gpd.read_file(f'zip://{input_filename}.zip!{NATURAL_EARTH_FILENAME}.shp')
     return df_naturalearth
 
 
 def output_to_cmf(df_output, df_output_reduced, cmf_path):
-    output_dir = os.path.join(cmf_path, CRASH_MOVE_MAIN_DIR, OUTPUT_DIR, datetime.date.today().strftime('%d%m'))
+    output_dir = os.path.join(cmf_path, CRASH_MOVE_MAIN_DIR, CRASH_MOVE_OUTPUT_DIR,
+                              OUTPUT_DIR, datetime.date.today().strftime('%Y%m%d'))
     try:
         os.mkdir(output_dir)
     except FileExistsError:
